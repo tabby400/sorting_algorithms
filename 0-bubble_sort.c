22c060300395bbdde36c1d9b6cc98571571a9bc6
tabby400 <tabbynargaret@gmail.com>
2023-07-24 05:06:07 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+/**
+ * int_swapping - function used in the swapping of ints
+ * in an array.
+ *
+ * @a: 1st int to be swapped.
+ * @b: 2nd int to  be swapped.
+ */
+
+void int_swapping(int *a, int *b)
+{
+	int hold;
+
+	hold = *a; /*to not loose a value*/
+	*a = *b;
+	*b = hold; /*takes a value*/
+}
+
+/**
+ * bubble_sort - function that is used to sort an array og ints
+ * in ascending order
+ *
+ * @array: this is an array containing the ints to be
+ * sorted
+ * @size: this is size of array and is to be printed after each 
+ * size is made
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t p;
+	size_t curr_size = size;
+	bool sorted = false;
+
+	if (!array || size < 2)
+	{
+		return;
+	}
+
+	while (sorted == false)/*ascendin order not gotten*/
+	{
+		sorted = true;
+		for (p = 0 ; p < curr_size - 1 ; p++)
+		{
+			if (array[p] > array[p + 1])
+			{
+				int_swapping(array + p, array + p + 1);
+				print_array(array, size);
+				sorted = false;
+			}
+		}
+		curr_size--;
+	}
+}
---
+++ sort.h
@@ -0,0 +1,42 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * enum  - creating enumaration to represent boolean
+ * data types
+*/
+
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void int_swapping(int *a, int *b);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
